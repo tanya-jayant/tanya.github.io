66111587ea73ac166e63d4de31e10c87674e64e9
unknown <tanyajayant27@gmail.com>
2018-03-27 15:05:16 +0530

    down arrow

--- assets/js/index.js
+++ assets/js/index.js
@@ -150,4 +150,17 @@ var Index = function() {
 			homeCarouselHandler();
 		}
 	};
+	$(document).ready(function(){
+		$("a").on('click', function(event) {
+		  if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+			  scrollTop: $(hash).offset().top
+			}, 800, function(){
+			  window.location.hash = hash;
+			});
+		  } 
+		});
+	  });
 }();
